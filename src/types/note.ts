240375f93dd7708accffbae2c2e0dc53366e3aaf
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-16 15:04:40 +0300

    Update note.ts

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,16 +1,7 @@
-export type Tag = "Work" | "Personal" | "Meeting" | "Shooping" | "Todo";
-
 export interface Note {
-  id: number;
-  title: string;
-  content: string;
-  createdAt: string;
-  updatedAt: string;
-  tag: Tag;
-}
-
-export interface NewNoteData {
-  title: string;
-  content: string;
-  tag: Tag;
+    id: number;
+    title: string;
+    content: string;
+    tag: string;
+    isArchived?: boolean;
 }
