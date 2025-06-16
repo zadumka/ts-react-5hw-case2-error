3ba2bcde0306a9e19acf9fbe303ffc2d71cac8a4
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-16 14:50:39 +0300

    Update Modal.tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,42 +1,34 @@
-import { useEffect } from 'react';
-import { createPortal } from 'react-dom';
-import NoteForm from '../NoteForm/NoteForm';
-import css from './NoteModal.module.css';
+import { createPortal } from "react-dom";
+import css from "./Modal.module.css";
+import { useEffect } from "react";
 
 interface ModalProps {
   onClose: () => void;
+  children: React.ReactNode;
 }
 
-export default function NoteModal({ onClose }: ModalProps) {
-  useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') onClose();
-    };
-    document.addEventListener('keydown', handleKeyDown);
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-      document.body.style.overflow = '';
-    };
-  }, [onClose]);
-
+export default function Modal({ onClose, children }: ModalProps) {
   const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (e.target === e.currentTarget) {
-      onClose();
-    }
+    if (e.target === e.currentTarget) onClose();
   };
+
+  useEffect(() => {
+    const handleEscClick = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+    document.addEventListener("keydown", handleEscClick);
+    return () => document.removeEventListener("keydown", handleEscClick);
+  }, [onClose]);
 
   return createPortal(
     <div
-      className={css.backdrop}
       onClick={handleBackdropClick}
+      className={css.backdrop}
       role="dialog"
       aria-modal="true"
     >
-      <div className={css.modal}>
-        <NoteForm onClose={onClose} />
-      </div>
+      <div className={css.modal}>{children}</div>
     </div>,
-    document.body,
+    document.body
   );
 }
